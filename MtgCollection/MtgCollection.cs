22198582c9275220f0c624908050a0e236a5ed2d
Reid Herrera <reidherrerabusiness@gmail.com>
2025-09-16 08:50:00 -0500

    completed the ability to add cards to the collection

--- MtgCollection/MtgCollection.cs
+++ MtgCollection/MtgCollection.cs
@@ -5,7 +5,16 @@ public class MtgCollection : IMtgCollection
 
   public void AddCardToCollection()
   {
+    System.Console.WriteLine("What is the name of the card you would like to add?");
+    var cardName = Console.ReadLine();
+    System.Console.WriteLine("What is the PSA grade on the card?");
+    var psaGrade = Console.ReadLine();
+    System.Console.WriteLine("What is the Ebay value of the card?");
+    var ebayValue = Console.ReadLine();
+    System.Console.WriteLine();
 
+    Collection.Add(new MtgCard(cardName, int.Parse(psaGrade), int.Parse(ebayValue)));
+    System.Console.WriteLine($"Awesome! {cardName} has been added to your collection! Your current collection is now worth {Collection.Sum(card => card.EbayValue)}\n");
   }
 
   public void SeeCollection()
